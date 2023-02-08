1fd865b799f2292d55d4040f66111e543796fad4
mstamenkovski27@gmail.com <mstamenkovski27@gmail.com>
2023-02-08 16:02:10 +0100

    homework---4

--- JS--Advanced--homework/homework---4/script.js
+++ JS--Advanced--homework/homework---4/script.js
@@ -1,9 +1,9 @@
-// const limitProductResultsURL = 'https://fakestoreapi.com/products?limit=5';
-const getAllProductsURL = 'https://fakestoreapi.com/products';
-const jeweleryURL = 'https://fakestoreapi.com/products/category/jewelery';
-const electronicsURL = 'https://fakestoreapi.com/products/category/electronics';
-const womensClothingURL = `https://fakestoreapi.com/products/category/women's clothing`;
-const mansClothingURL = `https://fakestoreapi.com/products/category/men's clothing`;
+const categoryUrls = [
+    'https://fakestoreapi.com/products/category/jewelery',
+    'https://fakestoreapi.com/products/category/electronics',
+    `https://fakestoreapi.com/products/category/women's clothing`,
+    `https://fakestoreapi.com/products/category/men's clothing`
+];
 
 const male = document.getElementById("male");
 const female = document.getElementById('female');
@@ -11,110 +11,61 @@ const jewelery = document.getElementById('jewelery-container');
 const electronics = document.getElementById('electronics-container');
 const mansClothingDiv = document.getElementById('mansClothing');
 const homeBTN = document.getElementById("goBack");
+const maleID = document.getElementById("maleID")
+const femaleID = document.getElementById("femaleID")
+const jeweleryID = document.getElementById("jeweleryID")
+const electronicsID = document.getElementById("electronicsID")
 
-//BTN
+// BTN
+const maleBTN = document.getElementById("maleBTN")
+const femaleBTN = document.getElementById("femaleBTN")
+const jeweleryBTN = document.getElementById("jeweleryBTN")
+const electronicsBTN = document.getElementById("electronicsBTN")
+
 const btn = document.getElementById("btn");
 const showCategory = document.getElementById("showCategory");
 const ul = document.getElementById("ul");
 
-
-
-fetch(jeweleryURL)
-    .then(res => res.json())
-    .then(json => {
-        json.forEach(element => {
-            const elementsHTML = cardInfoJewelery(element);
-            jewelery.innerHTML += elementsHTML;
-        });
-    })
-
-fetch(electronicsURL)
-    .then(res => res.json())
-    .then(json => {
-        json.forEach(element => {
-            const elementsHTML = cardInfoElectronics(element);
-            electronics.innerHTML += elementsHTML;
-        });
-    })
-
-fetch(womensClothingURL)
-    .then(res => res.json())
-    .then(json => {
-        json.forEach(element => {
-            const elementsHTML = cardInfoFemale(element);
-            female.innerHTML += elementsHTML;
-        });
-    })
-
-fetch(mansClothingURL)
-    .then(res => res.json())
-    .then((json) => {
-        json.forEach(element => {
-            const elementsHTML = cardInfoMale(element);
-            male.innerHTML += elementsHTML;
-        });
-    })
-
-function cardInfoMale(element) {
-    const informations = `
-        <div clss='card' id="maleConfig">
+function generateProductHTML(element) {
+    return `
+      <div class='card' id="electroConfig">
         <div id='img-config'>
-        <img id='img' src=${element.image} alt="img">
+          <img id='img' src=${element.image} alt="img">
         </div>
-        <div id='mansClothing'>
-        ${element.title}
+        <div>
+          ${element.title}
         </div>
-        <input type="button" value = "Buy now!" name="card" id="buyBTN">
-        </div>
-    `
-    return informations;
+        <input type="button" value="Buy now!" name="card" id="buyBTN">
+      </div>
+    `;
 }
 
-function cardInfoFemale(element) {
-    const informations = `
-        <div clss='card' id="femaleConfig">
-        <div id='img-config'>
-        <img id='img' src=${element.image} alt="img">
-        </div>
-        <div id='femaleClothing'>
-        ${element.title}
-        </div>
-        <input type="button" value = "Buy now!" name="card" id="buyBTN">
-        </div>
-    `
-    return informations;
+async function fetchCategories() {
+    const allProducts = await Promise.all(categoryUrls.map(async url => {
+        const res = await fetch(url);
+        return await res.json();
+    }));
+
+    const products = allProducts.flat();
+    male.innerHTML = products
+        .filter(product => product.category === `men's clothing`)
+        .map(product => generateProductHTML(product))
+        .join('');
+    female.innerHTML = products
+        .filter(product => product.category === `women's clothing`)
+        .map(product => generateProductHTML(product))
+        .join('');
+    jewelery.innerHTML = products
+        .filter(product => product.category === 'jewelery')
+        .map(product => generateProductHTML(product))
+        .join('');
+    electronics.innerHTML = products
+        .filter(product => product.category === 'electronics')
+        .map(product => generateProductHTML(product))
+        .join('');
 }
 
-function cardInfoJewelery(element) {
-    const informations = `
-        <div clss='card' id="jeweleryConfig">
-        <div id='img-config'>
-        <img id='img' src=${element.image} alt="img">
-        </div>
-        <div id='jewelery'>
-        ${element.title}
-        </div>
-        <input type="button" value = "Buy now!" name="card" id="buyBTN">
-        </div>
-    `
-    return informations;
-}
-
-function cardInfoElectronics(element) {
-    const informations = `
-        <div clss='card' id="electroConfig">
-        <div id='img-config'>
-        <img id='img' src=${element.image} alt="img">
-        </div>
-        <div id='electronics'>
-        ${element.title}
-        </div>
-        <input type="button" value = "Buy now!" name="card" id="buyBTN">
-        </div>
-    
-    `
-    return informations;
-}
+fetchCategories();
 
 showCategory.addEventListener('click', () => {
     if (ul.style.display === 'none') {
@@ -122,4 +73,45 @@ showCategory.addEventListener('click', () => {
     } else {
         ul.style.display = 'none'
     }
+});
+
+maleBTN.addEventListener("click", () => {
+    male.style.display = "flex";
+    female.style.display = "none";
+    jewelery.style.display = "none";
+    electronics.style.display = "none";
+    maleID.style.display = "flex";
+    femaleID.style.display = "none";
+    jeweleryID.style.display = "none";
+    electronicsID.style.display = "none";
+});
+femaleBTN.addEventListener("click", () => {
+    male.style.display = "none";
+    female.style.display = "flex";
+    jewelery.style.display = "none";
+    electronics.style.display = "none";
+    maleID.style.display = "none";
+    femaleID.style.display = "flex";
+    jeweleryID.style.display = "none";
+    electronicsID.style.display = "none";
+});
+jeweleryBTN.addEventListener("click", () => {
+    male.style.display = "none";
+    female.style.display = "none";
+    jewelery.style.display = "flex";
+    electronics.style.display = "none";
+    maleID.style.display = "none";
+    femaleID.style.display = "none";
+    jeweleryID.style.display = "flex";
+    electronicsID.style.display = "none";
+});
+electronicsBTN.addEventListener("click", () => {
+    male.style.display = "none";
+    female.style.display = "none";
+    jewelery.style.display = "none";
+    electronics.style.display = "flex";
+    maleID.style.display = "none";
+    femaleID.style.display = "none";
+    jeweleryID.style.display = "none";
+    electronicsID.style.display = "flex";
 });
